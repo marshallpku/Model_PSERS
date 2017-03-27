1b6e81e23307454f2fb55cab9aefa7c8121a2eec
Yimeng Yin <marshallpku@gmail.com>
2017-03-27 11:10:59 -0400

    import data

---
+++ PSERS_0_RunControl.R
@@ -0,0 +1,180 @@
+# Run control file of the PSERS model
+
+
+#********************************************************************************
+#                            Packages and tools ####
+#********************************************************************************
+
+rm(list = ls())
+gc()
+
+library(knitr)
+library(data.table)
+library(gdata) # read.xls
+library(plyr)
+library(dplyr)
+options(dplyr.print_min = 100) # default is 10
+options(dplyr.print_max = 100) # default is 20
+library(ggplot2)
+library(magrittr)
+library(tidyr) # gather, spread
+library(foreach)
+library(doParallel)
+library(microbenchmark)
+library(readxl)
+library(stringr)
+library(zoo)
+library("readxl")
+library("XLConnect") # slow but convenient because it reads ranges; NOTE: I had to install Java 64-bit on Windows 10 64-bit to load properly
+# library(xlsx)
+library("btools")
+options(dplyr.print_min = 60) # default is 10
+
+
+source("Functions.R")
+
+
+#********************************************************************************
+#                             Development Notes ####
+#********************************************************************************
+
+# Road map for model files (all with suffix "PSERS_" and extension ".R")
+
+  # Master files:
+  # Master_singleTier
+  # Master_allTiers
+  
+  # Data_RP2000
+  # Data_PlanInfo 
+  # Data_ImportMemberData
+  
+  # Model_decrements
+  # Model_InvReturns
+  # Model_PrepData
+  # Model_Demographics
+  # Model_ContingentAnnuity
+  # Model_IndivLiab
+  # Model_AggLiab
+  # Model_Sim
+
+
+
+
+
+#### Model Parameters ####
+#********************************************************************************
+folder_run <- "."
+filename_RunControl <- dir(folder_run, pattern = "^RunControl")
+path_RunControl <- paste0(folder_run, "/" ,filename_RunControl)
+
+# Import global parameters
+runList <- read_excel(path_RunControl, sheet="params", skip = 0) %>% filter(!is.na(runname), include == 1)
+runList
+
+# Import return scenarios
+returnScenarios <- read_excel(path_RunControl, sheet="returns", skip = 0) %>% filter(!is.na(scenario))
+
+# Import global parameters
+Global_paramlist <- read_excel(path_RunControl, sheet="GlobalParams") %>% filter(!is.na(init.year)) %>% 
+                 as.list
+
+
+#### Run Models and Save  ####
+#********************************************************************************
+
+folder_save <- "Results/"
+
+
+
+#####  Run Model ####
+#*********************************************************************************************************
+
+
+for(runName in runList$runname ){
+  
+  #runName <- "RS1"
+  
+  paramlist <- get_parmsList(runList, runName)
+  
+  paramlist$simTiers <- "separate"  # "joint"(defult) or "separate"
+
+  if(paramlist$nyear.override != 0) Global_paramlist$nyear <- paramlist$nyear.override
+  
+
+  # Global_paramlist (Specified in RunControl.xlsx)
+  
+    # init.year = 2015,
+    # nyear     = 30,
+    # nsim      = 2000,
+    # ncore     = 4,
+    # 
+    # min.ea    = 20,
+    # max.ea    = 74, # Retirement rate is 100% at age 75 
+    # 
+    # min.age   = 20,
+    # max.age   = 115 # Need to set mortality at age 115 to 1
+
+  
+  
+ # Benefit provisions
+  paramlist$r.min  <- 45 # this is not required age of retirement benefit. 
+  paramlist$r.max  <- 74 
+  
+  # paramlist$r.full <- 50 # age at which vested terms are assumed to retire(Temp, should use r.vben)
+  # paramlist$r.vben <- 50 # age at which vested terms are assumed to retire.
+  
+
+ # Funding policy 
+  paramlist$smooth_method <- "method1"
+  paramlist$salgrowth_amort <- 0.055
+  paramlist$amort_type <- "open"
+  
+  paramlist$s.lower <- -Inf # No corridor for AA
+  paramlist$s.upper <- Inf
+ 
+  paramlist$actuarial_method <- "EAN.CP" 
+  
+  paramlist$init_EAA <- "MA"
+
+  
+ # Economic assumption
+  paramlist$infl <- 0.03
+  paramlist$prod <- 0.008
+  paramlist$startingSal_growth <- 0.038
+
+  
+ # Demographic
+  paramlist$Grouping    <- "fillin"
+  paramlist$newEnt_byTier <- c(t1 = 0, t2 = 0, t3 = 0, t4 = 0, t5 = 0, t6 = 1)
+
+  paramlist$pct.ca.M <-  0.8 # proportion of males who opt for ca upon retirement
+  paramlist$pct.ca.F <-  0.6
+ 
+   
+ # Investment returns
+  paramlist$seed <- 1234
+
+
+  # Parameters derived from the parameter list above. 
+  paramlist$range_ea = with(Global_paramlist, min.ea:max.ea)
+  paramlist$range_age = with(Global_paramlist, min.age:max.age)
+  paramlist$range_age.r = with(paramlist, r.min:r.max)
+  paramlist$v     = with(paramlist, 1/(1 + i))
+  
+  
+
+  if(paramlist$tier == "sumTiers"){
+    source("LAFPP_0_Master_allTiers.R")
+    save(outputs_list, file = paste0(folder_save, "results_",  paramlist$tier, "_", runName, ".RData"))
+
+  } else {
+    Tier_select <- paramlist$tier
+    source("LAFPP_0_Master_singleTier.R")
+    save(outputs_list, file = paste0(folder_save, "results_",  paramlist$tier, runName, ".RData"))
+  }
+
+}
+
+
+
+
---
+++ PSERS_Data_PlanInfo_AV2015.R
@@ -0,0 +1,148 @@
+# This script imports plan information from PlanInfo-PSERS_AV2015.xlsx
+
+# This script performs necessary transformations (eg. truncation, expansion, imputation) to the original data, 
+# but do not create new variables (eg. computing weighted average. )
+
+# Data sources:
+  # Data_inputs/PSERS_PlanInfo_AV2015.xlsx
+
+# List of outputs
+  # mortality_PSERS, 
+  # retRates, 
+  # termRates, 
+  # disbRates, 
+  # bfactors, 
+  # salgrowth
+  # tier.param
+  # init_amort_raw
+
+# Output file:
+  # Data_inputs/LAFPP_PlanInfo.RData
+
+
+#### To do list
+# 1. More smoothed imputation of decrements
+
+
+
+# plan information file:
+file_planInfo <- "Data_inputs/PSERS_PlanInfo_AV2015.xlsx"
+
+
+#*********************************************************************************************************
+#                      ## Tools ####
+#*********************************************************************************************************
+
+
+
+
+
+#*********************************************************************************************************
+#                           ## Mortality tables ####
+#*********************************************************************************************************
+
+# mortality for active members
+# mortality for retirees and beneficiaries 
+# mortality for disabled
+
+mortality_PSERS <- data.frame(age = 20:120) %>% 
+  left_join(read_ExcelRange(file_planInfo, sheet="Death_decAct", "B2", "B3", colTypes="numeric")) %>% 
+  left_join(read_ExcelRange(file_planInfo, sheet="Death_decRet", "B2", "B3", colTypes="numeric")) %>% 
+  mutate_all(na2zero)
+
+# mortality_PSERS
+
+
+#*********************************************************************************************************
+#                      ## Retirement rates  ####
+#*********************************************************************************************************
+retRates <- read_ExcelRange(file_planInfo, sheet="Ret_dec", "B2", "B3", colTypes="numeric")
+
+# Early retirement rates
+# Superannuation retirement rates
+
+retRates <- data.frame(age = 20:74) %>% 
+  left_join(read_ExcelRange(file_planInfo, sheet="Ret_dec", "B2", "B3", colTypes="numeric")) %>% 
+  mutate_all(na2zero)
+
+#retRates
+
+
+#*********************************************************************************************************
+#                      ## Termination rates  ####
+#*********************************************************************************************************
+
+# Term rates with yos < 5
+# Term rates with 5 <= yos < 10
+# Term rates with yos >= 10
+
+termRates_raw <- data.frame(age = 20:74) %>% 
+  left_join(read_ExcelRange(file_planInfo, sheet="Term_dec", "B2", "B3", colTypes="numeric")) %>% 
+  mutate_all(na2zero)
+# termRates_raw 
+
+
+termRates <- expand.grid(ea = 20:74, age = 20:74) %>% 
+  mutate(yos = age - ea) %>% 
+  filter(age >= ea) %>% 
+  arrange(ea, age) %>% 
+  left_join(termRates_raw) %>% 
+  mutate(qxt.male = ifelse(yos < 5, qxt.male.yosLT5,
+                    ifelse(yos < 10, qxt.male.yosLT10,
+                    ifelse(yos >=10, qxt.male.yosGTE10, 0))),
+         
+         qxt.female = ifelse(yos < 5, qxt.female.yosLT5,
+                      ifelse(yos < 10, qxt.female.yosLT10,
+                      ifelse(yos >=10, qxt.female.yosGTE10, 0)))
+  ) %>% 
+  select(age, ea, yos, qxt.male, qxt.female) %>% 
+  mutate_all(na2zero)
+
+#termRates
+
+
+
+#*********************************************************************************************************
+#                      ## disability rates     ####
+#*********************************************************************************************************
+
+disbRates <- data.frame(age = 20:74) %>% 
+  left_join(read_ExcelRange(file_planInfo, sheet="Disb_dec", "B2", "B3", colTypes="numeric")) %>% 
+  mutate_all(na2zero)
+
+#disbRates
+
+
+
+#*********************************************************************************************************
+#                      ## Salary growth rates  ####
+#*********************************************************************************************************
+
+salgrowth <- read_ExcelRange(file_planInfo, sheet="SalaryGrowth", "B2", "B3", colTypes="numeric")
+
+
+
+#*********************************************************************************************************
+#                      ## Tier specific parameters ####
+#*********************************************************************************************************
+
+tier.param <- read_ExcelRange(file_planInfo, sheet="Tier.param", colTypes="character") %>% 
+  mutate_at(vars(-tier), funs(as.numeric))
+
+row.names(tier.param) <- tier.param$tier
+
+tier.param
+
+#*********************************************************************************************************
+#                      ## Initial unrecognized return  ####
+#*********************************************************************************************************
+
+init_unrecReturns.unadj <- read_ExcelRange(file_planInfo, sheet = "Init_unrecReturn", colTypes="numeric") 
+# init_unrecReturns.unadj
+
+save(mortality_PSERS, retRates, termRates, disbRates, salgrowth, tier.param, init_unrecReturns.unadj,
+     file  = "Data_inputs/LAFPP_PlanInfo_2016.RData")
+
+
+
+
